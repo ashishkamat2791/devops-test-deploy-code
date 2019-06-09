2557740710c9b4a74dbac6195bac213bfa26de12
ashishkamat2791 <ashishkamat2791@gmail.com>
2019-06-09 11:30:01 +0530

    added code for single node js app deployment

---
+++ Solution-1/web-app/vars.tf
@@ -0,0 +1,23 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-08d658f84a6d84a80"
+    us-west-2 = "ami-005bdb005fb00e791"
+    eu-west-1 = "ami-08d658f84a6d84a80"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
---
+++ Solution-1/web-app/web-dev-instance.tf
@@ -0,0 +1,27 @@
+ resource "aws_key_pair" "mykey" {
+  key_name = "mykey"
+  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
+}
+resource "aws_instance" "buildit_dev_node" {
+    instance_type = "t2.micro"
+    ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+    key_name = "${aws_key_pair.mykey.key_name}"
+
+  provisioner "local-exec" {
+    command = <<EOD
+    apt-get install curl python-software-properties
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    apt-get install nodejs
+    node --version
+    npm --version
+EOD
+
+  }
+  connection {
+    host = "${self.public_ip}"
+    user = "${var.INSTANCE_USERNAME}"
+    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
+  } 
+  
+} 
+  
